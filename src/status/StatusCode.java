98361a1c03b21b8bdcbf505ba5a92ed27ae031ef
IsraBahar <baharis@sheridancollege.ca>
2024-06-19 17:47:41 -0400

    partTime Branch

---
+++ src/status/StatusCode.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package status;
+
+/**
+ *
+ * @author issra
+ */
+
+/**
+ * Enum representing the status codes
+ */
+public enum StatusCode {
+    ZERO, ONE, TWO, THREE;
+}
